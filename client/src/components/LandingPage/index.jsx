fb04b4f49aa6cdf2d260b677872348849c45b1ea
Kauã Steliano <stelianok@gmail.com>
2022-06-04 16:47:08 -0300

    Created landing page structure

--- client/src/components/LandingPage/index.jsx
+++ client/src/components/LandingPage/index.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import {Title, Wrapper} from "./styles";
+
+import AsteroidCard from "../AsteroidCard";
+
+import {
+  Container,
+  Header,
+  Menu,
+  Logo,
+  PageContainer,
+  HomeButton,
+  FeedButton,
+  HeaderBody,
+  Body
+} from "./styles";
 
 export default function LandingPage(){
   return(
-    <Wrapper>
-    <Title>
-      Landing Page
-    </Title>
-  </Wrapper>
+   <Container>
+     <Header>
+       <Menu>
+       <Logo>Assteroid</Logo>
+       <PageContainer>
+         <HomeButton>Home</HomeButton>
+         <FeedButton>Feed</FeedButton>
+       </PageContainer>
+       </Menu>
+       <HeaderBody>
+         <AsteroidCard/>
+       </HeaderBody>
+     </Header>
+     <Body>
+       
+     </Body>
+   </Container>
   )
 }
