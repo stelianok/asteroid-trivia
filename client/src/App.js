4853f005f51fc450847e9669b85ada440e2524f4
Kauã Steliano <stelianok@gmail.com>
2022-06-04 14:59:14 -0300

    Added api Call TO NASA API

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+const baseURL = "https://api.nasa.gov/neo/rest/v1/neo/browse/?api_key=BATATA";
+
 
 function App() {
+  function GetRandomPlanet(){
+    console.log("planeta aleatório")
+  }
+
+  const [meteor, setMeteor] = useState();
+
+  useEffect(() => { axios.get(baseURL).then((response) => {
+
+      setMeteor(response.data.near_earth_objects[0]);
+      console.log(response.data.near_earth_objects[0]);
+      GetRandomPlanet();
+
+    }).catch(() => {
+      console.log("Deu erro na api")
+    })
+  }, []);
+ 
   return (
     <div>
-     asteróide ⭐⭐⭐☄️
+      <h1> {meteor ? (meteor.name) : ("batata")}</h1>
     </div>
   );
 }
