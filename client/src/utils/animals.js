b916df6a492ad3f8ebb9e30476b25b1146ac9128
Kauã Steliano <stelianok@gmail.com>
2022-06-04 23:34:30 -0300

    Created Animals arrays
    Co-authored-by: Vinicius Fiedler <viniFiedler@users.noreply.github.com>

---
+++ client/src/utils/animals.js
@@ -0,0 +1,168 @@
+
+const animalsSpeed = [
+    {
+      "name": "Bald Eagle",
+      "icon": "🦅",
+      "speed": 160
+    },
+    {
+      "name": "Parrot",
+      "icon": "🦜",
+      "speed": 153
+    }
+    ,
+    {
+      "name": "Dove",
+      "icon": "🕊️",
+      "speed": 146
+    }
+    ,
+    {
+      "name": "Horse",
+      "icon": "🐎",
+      "speed": 88
+    }
+    ,
+    {
+      "name": "Kangaroo",
+      "icon": "🦘",
+      "speed": 70
+    }
+     ,
+    {
+      "name": "Zebra",
+      "icon": "🦓",
+      "speed":  64
+    }
+    ,
+    {
+      "name": "Wolf",
+      "icon": "🐺",
+      "speed": 55
+    }
+    ,
+    {
+      "name": "Shark",
+      "icon": "🦈",
+      "speed": 50
+    }
+    ,
+    {
+        "name": "Blue Whale",
+        "icon": "🐋",
+        "speed": 47
+    }
+    ,
+    {
+      "name": "Turtle",
+      "icon": "🐢",
+      "speed": 35
+    }
+    ,
+    {
+      "name": "Pig",
+      "icon": "🐖",
+      "speed": 18
+    }
+    ,
+    {
+      "name": "Fly",
+      "icon": "🪰",
+      "speed": 6
+    }
+    ,
+    {
+      "name": "Snail",
+      "icon": "🐌",
+      "speed":  0.05
+    }
+  ];
+  
+  const animalsWidth = [
+    {
+      "name": "Blue Whale",
+      "icon": "🐋",
+      "width": 24
+    },
+    ,
+    {
+      "name": "Shark",
+      "icon": "🦈",
+      "width": 6.4
+    }
+    ,
+    {
+      "name": "Giraffe",
+      "icon": "🦒",
+      "width": 6
+    }
+    ,
+    {
+      "name": "Elephant",
+      "icon": "🐘",
+      "width": 6 
+    }
+    ,
+    {
+      "name": "Octopus",
+      "icon": "🐙",
+      "width": 5
+    }
+    ,
+    {
+      "name": "White rhino",
+      "icon": "🦏",
+      "width": 4
+    }
+    ,
+    {
+      "name": "Hipoppotamus",
+      "icon": "🦛",
+      "width": 3.6
+    }
+    ,
+    {
+      "name": "Indian Peafowl",
+      "icon": "🦚",
+      "width":  3
+    }
+    ,
+    {
+      "name": "Kangaroo",
+      "icon": "🦘",
+      "width": 2.8
+    }
+    ,
+    {
+      "name": "Royal Bengal Tiger",
+      "icon": "🐅",
+      "width": 2.6
+    }
+    ,
+    {
+      "name": "Pig",
+      "icon": "🐖",
+      "width": 1.6
+    }
+    ,
+    {
+      "name": "Monkey",
+      "icon": "🐒",
+      "width": 1.1
+    }
+    ,
+    {
+      "name": "Rat",
+      "icon": "🐀",
+      "width": 0.2
+    }
+    ,
+    {
+      "name": "Ant",
+      "icon": "🐜",
+      "width": 0.072
+    }
+  ];
+
+
+export { animalsSpeed, animalsWidth};
