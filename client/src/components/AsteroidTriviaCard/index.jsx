9afdbf65579068fbc33aedf10b7700c1872240ee
Kauã Steliano <stelianok@gmail.com>
2022-06-05 17:38:27 -0300

    Removed comments, now asteroidTriviaCard receives only one asteroid.

--- client/src/components/AsteroidTriviaCard/index.jsx
+++ client/src/components/AsteroidTriviaCard/index.jsx
@@ -45,6 +45,8 @@ export default function AsteroidTriviaCard({ asteroidData, triviaType = "speed" 
 
     console.warn(`Selected trivia asteroid ${randomFormattedAsteroid.name}`);
 
+    setAsteroid(randomFormattedAsteroid);
+
   }, []);
 
   useEffect(() => {
