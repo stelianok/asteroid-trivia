1983599108ea50ac5e0f12f5fb99ee6551f334ea
Kauã Steliano <stelianok@gmail.com>
2022-06-05 14:18:03 -0300

    Remove plug that doesn't work on older versions of nodeJS

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
-import macrosPlugin from "vite-plugin-babel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), macrosPlugin()],
+  plugins: [react()],
   define: {
     "process.env": process.env,
   },
