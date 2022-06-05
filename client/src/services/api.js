da2ddb5622f29b35b01ed6fec7857b71a44f2029
Cassiano Rodrigues <66069923+kszinhu@users.noreply.github.com>
2022-06-05 10:41:53 -0300

    chore: changed react-scripts to viteJS
    + Removido o React-Scripts para o uso de ViteJS
      - Imaginando que vai ser necessário para o envio do projeto

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_NASA_API_BASE_URL,
+  baseURL: import.meta.env.VITE_NASA_API_BASE_URL,
   timeout: 10000,
   params: {
-    api_key: process.env.REACT_APP_NASA_API_KEY,
+    api_key: import.meta.env.VITE_NASA_API_KEY,
   },
 });
 
