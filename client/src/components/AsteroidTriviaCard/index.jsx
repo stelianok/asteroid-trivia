fd5e5fd5adfde5076f287b28526f78a89e0e29e3
Kauã Steliano <stelianok@gmail.com>
2022-06-04 22:12:07 -0300

    Created basic structure for Asteroid Trivia

---
+++ client/src/components/AsteroidTriviaCard/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default function AsteroidTriviaCard() {
+  return (
+    <Container>
+      <strong>Asteroid mano </strong>
+    </Container>
+    
+  )
+}
